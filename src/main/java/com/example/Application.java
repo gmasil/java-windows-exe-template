14fce96f7a7731ccac32caacae7c3175fe17284f
gmasil <54176035+gmasil@users.noreply.github.com>
2023-02-25 12:16:23 +0100

    initial commit

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.io.IOException;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World");
+        System.out.println("Press enter to continue...");
+        System.in.read();
+    }
+}
